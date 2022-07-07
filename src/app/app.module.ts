25f8647c67a8032657d83a6f671787e61743ef7d
TheerthaPraveen <theerthapraveen.n@gmail.com>
2022-07-07 17:25:18 +0530

    search-places

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
 import { ViewDestinationsComponent } from './view-destinations/view-destinations.component';
+import { SearchPlacesComponent } from './search-places/search-places.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TravelRegistrationComponent,
-    ViewDestinationsComponent
+    ViewDestinationsComponent,
+    SearchPlacesComponent
   ],
   imports: [
     BrowserModule,
